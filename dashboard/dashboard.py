7f32aa6b1f4e1246daa7311aa987b2af595461b7
Dafa Alvian <dafaalvian17@gmail.com>
2026-04-28 17:54:03 +0700

    Finalisasi Dashboard e commerce

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,10 +6,8 @@ st.set_page_config(page_title="Dashboard E-Commerce", layout="wide")
 
 st.title("Dashboard Analisis E-Commerce")
 
-
 df = pd.read_csv('dashboard/main_data.csv')
 df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
-
 
 st.sidebar.header("Informasi Dashboard")
 
@@ -41,42 +39,33 @@ st.sidebar.write("Total Data:", len(df))
 st.sidebar.write("Total Pelanggan:", df['customer_unique_id'].nunique())
 st.sidebar.write("Total Produk:", df['product_id'].nunique())
 
-
 col1, col2, col3 = st.columns(3)
 
 col1.metric("Total Transaksi", df['order_id'].nunique())
 col2.metric("Total Pelanggan", df['customer_unique_id'].nunique())
 col3.metric("Total Produk", df['product_id'].nunique())
 
-
 product_sales = df.groupby('product_id')['order_item_id'].count().sort_values(ascending=False).head(10)
-
 top_customers = df.groupby('customer_unique_id')['order_id'].nunique().sort_values(ascending=False).head(10)
-
 category_sales = df.groupby('product_category_name')['order_item_id'].count().sort_values(ascending=False).head(10)
-
 
 date_range = (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days
 
 if date_range <= 60:
     orders = df.groupby(df['order_purchase_timestamp'].dt.date)['order_id'].nunique()
-    x_label = "Tanggal (Harian)"
+    x_label = "Tanggal"
 else:
     orders = df.groupby(df['order_purchase_timestamp'].dt.to_period('M'))['order_id'].nunique()
     orders.index = orders.index.to_timestamp()
     x_label = "Bulan"
 
-
 st.header("Analisis Produk")
 
 fig1, ax1 = plt.subplots(figsize=(8,4))
-product_sales.plot(kind='bar', ax=ax1)
-ax1.set_ylabel("Jumlah Terjual")
-plt.xticks(rotation=45)
-plt.grid(axis='y', linestyle='--', alpha=0.5)
+product_sales.sort_values().plot(kind='barh', ax=ax1)
+ax1.set_xlabel("Jumlah Terjual")
 plt.tight_layout()
 st.pyplot(fig1)
-
 
 st.header("Analisis Transaksi")
 
@@ -89,28 +78,21 @@ plt.grid(axis='y', linestyle='--', alpha=0.5)
 plt.tight_layout()
 st.pyplot(fig2)
 
-
 st.header("Analisis Pelanggan")
 
 fig3, ax3 = plt.subplots(figsize=(8,4))
-top_customers.plot(kind='bar', ax=ax3)
-ax3.set_ylabel("Jumlah Transaksi")
-plt.xticks(rotation=45)
-plt.grid(axis='y', linestyle='--', alpha=0.5)
+top_customers.sort_values().plot(kind='barh', ax=ax3)
+ax3.set_xlabel("Jumlah Transaksi")
 plt.tight_layout()
 st.pyplot(fig3)
-
 
 st.header("Kategori Produk")
 
 fig4, ax4 = plt.subplots(figsize=(8,4))
-category_sales.plot(kind='bar', ax=ax4)
-ax4.set_ylabel("Jumlah Terjual")
-plt.xticks(rotation=45)
-plt.grid(axis='y', linestyle='--', alpha=0.5)
+category_sales.sort_values().plot(kind='barh', ax=ax4)
+ax4.set_xlabel("Jumlah Terjual")
 plt.tight_layout()
 st.pyplot(fig4)
-
 
 st.markdown("---")
 st.subheader("Sample Data")
